805512b5ff3d1e67f50869d7c6e3743a2322128b
Jitendra Adhikari <jiten.adhikary@gmail.com>
2019-12-21 08:12:54 +0700

    test: add highlighter test

---
+++ tests/HighlighterTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Ahc\CliSyntax\Test;
+
+use Ahc\CliSyntax\Highlighter;
+use PHPUnit\Framework\TestCase;
+
+class HighlighterTest extends TestCase
+{
+    public function testHighlightCode()
+    {
+        $code = (new Highlighter)->highlight('<?php echo "Hello world!";');
+
+        $this->assertSame(
+            '[0;32;40m<?php [0m[0;36;40mecho [0m[0;33;40m"Hello world!"[0m[0;36;40m;[0m',
+            $code
+        );
+    }
+
+    public function testHighlightFile()
+    {
+        $code = Highlighter::for(__DIR__ . '/../example.php')->highlight();
+
+        $this->assertSame(\file_get_contents(__DIR__ . '/example.phps'), $code);
+    }
+
+    /**
+     * @expectedException \InvalidArgumentException
+     * @expectedExceptionMessage The given file doesnot exist or is unreadable.
+     */
+    public function testHighlightFileThrows()
+    {
+        Highlighter::for(__DIR__ . '/' . rand());
+    }
+}
