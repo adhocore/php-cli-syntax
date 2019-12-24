8f88fa92b805492a196d3f594a15e10b05e8d039
Jitendra Adhikari <jiten.adhikary@gmail.com>
2019-12-25 06:56:53 +0700

    chore: add export example

--- example.php
+++ example.php
@@ -9,16 +9,23 @@
  * Licensed under MIT license.
  */
 
+use Ahc\CliSyntax\Exporter;
 use Ahc\CliSyntax\Highlighter;
 
+require_once __DIR__ . '/src/Pretty.php';
+require_once __DIR__ . '/src/Exporter.php';
 require_once __DIR__ . '/src/Highlighter.php';
 
 // Highlight code
-echo (new Highlighter("<?php echo 'Hello world!'; ?>\n"))->highlight();
+echo (new Highlighter("<?php echo 'Hello world!'; ?>" . PHP_EOL))
+    ->highlight();
 
 // Highlight file
 echo Highlighter::for(__FILE__)->highlight();
 
+// Export highlighted file as image
+Exporter::for(__FILE__)->export(__DIR__ . '/example.png');
+
 // Magic string works too:
-// echo new Highlighter('<?php echo "Hello world!";');
+echo new Highlighter('<?php echo "Hello Again!";' . PHP_EOL);
 // echo Highlighter::for(__FILE__);
