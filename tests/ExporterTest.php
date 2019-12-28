0aa18f1ff84fb8e9e521d6d74884744675d89062
Jitendra Adhikari <jiten.adhikary@gmail.com>
2019-12-28 13:31:40 +0700

    test: exporter

---
+++ tests/ExporterTest.php
@@ -0,0 +1,61 @@
+<?php
+
+/*
+ * This file is part of the CLI-SYNTAX package.
+ *
+ * (c) Jitendra Adhikari <jiten.adhikary@gmail.com>
+ *     <https://github.com/adhocore>
+ *
+ * Licensed under MIT license.
+ */
+
+namespace Ahc\CliSyntax\Test;
+
+use Ahc\CliSyntax\Exporter;
+use PHPUnit\Framework\TestCase;
+
+class ExporterTest extends TestCase
+{
+    protected $out = __DIR__ . '/export.png';
+    protected $ref = __DIR__ . '/../example.png';
+
+    public function setUp()
+    {
+        if (\is_file($this->out)) {
+            @unlink($this->out);
+        }
+    }
+
+    public function testExport()
+    {
+        Exporter::for(__DIR__ . '/../example.php')->export($this->out);
+
+        $this->assertFileExists($this->out, 'It should exports to given output path');
+        $this->assertSame(\file_get_contents($this->ref), \file_get_contents($this->out));
+    }
+
+    /**
+     * @expectedException \InvalidArgumentException
+     * @expectedExceptionMessage The output path doesnot exist.
+     */
+    public function testExportThrows()
+    {
+        Exporter::for(__DIR__ . '/../example.php')->export(__DIR__ . '/no/dir/file.png');
+    }
+
+    public function testSetOptions()
+    {
+        Exporter::for(__DIR__ . '/../example.php')->export($this->out, ['font' => 'ubuntu', 'size' => 16]);
+
+        $this->assertSame(\file_get_contents($this->ref), \file_get_contents($this->out));
+    }
+
+    /**
+     * @expectedException \InvalidArgumentException
+     * @expectedExceptionMessage The given font doesnot exist.
+     */
+    public function testSetOptionsThrows()
+    {
+        Exporter::for(__DIR__ . '/../example.php')->export($this->out, ['font' => 'invalid']);
+    }
+}
