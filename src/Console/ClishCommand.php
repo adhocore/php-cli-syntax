cef8caa6612e68b4442cf77c0028694d02190adb
Jitendra Adhikari <jiten.adhikary@gmail.com>
2019-12-30 21:18:06 +0700

    fix(clish.cmd): usage msg, font path in phar

--- src/Console/ClishCommand.php
+++ src/Console/ClishCommand.php
@@ -25,17 +25,17 @@ class ClishCommand extends Command
         parent::__construct('clish', 'PHP CLI syntax highlight and/or export.');
 
         $this
-            ->option('-o --output', 'Output filepath where PNG image is exported')
-            ->option('-e --echo', 'Forces echo to STDOUT when --output is passed')
+            ->option('-o --output', 'Output filepath where PNG image is exported', null, '')
+            ->option('-e --echo', 'Forces echo to STDOUT when --output is passed', null, '')
             ->option('-f --file', \implode("\n", [
                 'Input PHP file to highlight and/or export',
                 '(will read from piped input if file not given)',
-            ]))
-            ->option('-F --font', 'Font to use for export to png')
+            ]), null, '')
+            ->option('-F --font', 'Font to use for export to png', null, '')
             ->usage(
                 '<bold>  $0</end> <comment>--file file.php</end> ## print<eol/>'
-                . '<bold>  cat file.php | $0</end> ## from piped stream<eol/>'
-                . '<bold>  $0</end> <comment>< file.php ## from redirected stdin<eol/>'
+                . '<comment>  cat file.php |</end> <bold>$0</end> ## from piped stream<eol/>'
+                . '<bold>  $0</end> <comment>< file.php</end> ## from redirected stdin<eol/>'
                 . '<bold>  $0</end> <comment>--file file.php --output file.png</end> ## export<eol/>'
                 . '<bold>  $0</end> <comment>--file file.php --output file.png --echo</end> ## print + export<eol/>'
                 . '<bold>  $0</end> <comment>-f file.php -o file.png -F dejavu</end> ## export in dejavu font<eol/>'
@@ -44,7 +44,7 @@ class ClishCommand extends Command
 
     public function interact(Interactor $io)
     {
-        if (\is_string($this->file)) {
+        if ($this->file) {
             return;
         }
 
@@ -92,6 +92,23 @@ class ClishCommand extends Command
             \mkdir(\dirname($this->output), 0755, true);
         }
 
-        (new Exporter($code))->export($this->output, \array_filter(['font' => $this->font]));
+        $options = ['font' => $this->fixFont($this->font ?: 'ubuntu')];
+
+        (new Exporter($code))->export($this->output, $options);
+    }
+
+    /** @codeCoverageIgnore */
+    protected function fixFont(string $font): string
+    {
+        if (\Phar::running()) {
+            $basename = \basename($font, '.ttf');
+            $pharfont = __DIR__ . "/../../font/$basename.ttf";
+
+            if (!\is_file($font) && \is_file($pharfont)) {
+                \copy($pharfont, $font = sys_get_temp_dir() . '/clish.ttf');
+            }
+        }
+
+        return $font;
     }
 }
